16917d5093ae9290a030916f52fa64536e903d9b
Nada Zaki <nadazaki019@gmail.com>
2023-01-03 12:01:43 +0200

    first commit

---
+++ gsm.py
@@ -0,0 +1,22 @@
+import time
+import serial
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+phone = serial.Serial("/dev/serial0",115200,timeout=1)
+
+try:
+    phone.write(b'ATZ\r')  # set to base user profile
+    #print(phone.readall())
+
+    phone.write(b"AT+CMGF=1\r")  # set messages to text format
+    #print(phone.readall())
+
+    while 1:
+        phone.write(b'AT+CMGL="ALL"\r')  # read all stored messages
+        data = phone.readall()
+        print(data)
+        time.sleep(5)
+finally:
+    phone.close()
